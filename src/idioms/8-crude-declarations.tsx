c1fba07e703133d037ea722c8be0e4c64446989b
Leonardo Galante <lndgalante@gmail.com>
2024-03-04 17:16:34 -0300

    fix: Improve calendar days to be dynamic

--- src/idioms/8-crude-declarations.tsx
+++ src/idioms/8-crude-declarations.tsx
@@ -1,5 +1,7 @@
 // ---------------------------------------------------------------------------------------------------------------------
 
+import { useMemo } from "react";
+
 /* Problem 👇
 
 export function CrudeDeclarations() {
@@ -15,21 +17,25 @@ export function CrudeDeclarations() {
 /* Solution 👇
 
 1) Mapping over calendarDays return a span element, which is not a valid child of the ol element, so we need an li element in this case
-2) We can move the calendarDays outside the component and use it as a constant, so it is not re-declared on every render
+2) Let's create a function that returns the current calendar days and memoize it to avoid unnecessary re-renders
 
-3) Optionally we can use MACRO_CASE for the calendarDays constant to indicate that it is a constant
-4) Optionally we can use the as const assertion to indicate that the array is a readonly tuple
-5) Also we can rename the val variable to day to make it more descriptive
 */
 
-const CALENDAR_DAYS = [
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30,
-] as const;
+
+function getCurrentCalendarDays() {
+  const date = new Date();
+  const [year, month] = [date.getFullYear(), date.getMonth()]
+
+  const daysInMonth = new Date(year, month + 1, 0).getDate();
+  return Array.from({ length: daysInMonth }, (_, i) => i + 1);
+}
 
 export function CrudeDeclarations() {
+  const calendarDays = useMemo(() => getCurrentCalendarDays(), []);
+
   return (
     <ol>
-      {CALENDAR_DAYS.map((day) => (
+      {calendarDays.map((day) => (
         <li key={day}>{day}</li>
       ))}
     </ol>
