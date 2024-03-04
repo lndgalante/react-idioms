ccb36d302d6632442028029b5df40cdcbe9013d4
Leonardo Galante <lndgalante@gmail.com>
2024-03-03 22:03:50 -0300

    feat: Add raw react project

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+// Import here any idioms for testing
+const App = () => null
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
